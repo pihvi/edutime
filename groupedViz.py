8dec0632dcfd2bb6609a4c0f2a0d592804d69f79
Antti Mattila <antti.mattila@reaktor.fi>
2018-11-29 11:16:08 +0200

    Save plots

--- groupedViz.py
+++ groupedViz.py
@@ -5,8 +5,17 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('./data/sigite2014-difficulty-data.csv', sep=';')
 spent = df.filter(regex='SECONDS_SPENT_ON_*', axis=1)
 
-for week in range(1, 7):
+
+def save_plot(name):
+    plt.savefig('plots/' + name + '.png')
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+
+for week in range(1, 2):
     prefix = 'SECONDS_SPENT_ON_viikko0' + str(week) + '_'
-    for col in df.filter(regex=prefix + '.*', axis=1).columns:
+    for col in df.filter(regex=prefix + '.*', axis=1).columns[:2]:
         name = col.replace(prefix, '')
-        print(df[col].hist())
+        df[col].hist()
+        save_plot(name)
