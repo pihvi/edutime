4a43b8b27f7121e8b2a14a691f9e9d8fe184b01a
Antti Mattila <antti.mattila@reaktor.fi>
2018-11-29 12:41:34 +0200

    Add median line

--- groupedViz.py
+++ groupedViz.py
@@ -15,17 +15,20 @@ def save_plot(title):
     return file
 
 
+max_weeks = 6
+max_assigments = 200
 with open('reports/seconds_spent.md', 'w') as report:
-    for week in range(1, 7):
+    for week in range(1, max_weeks + 1):
         report.write('### Week ' + str(week) + ' \n')
         prefix = 'SECONDS_SPENT_ON_viikko0' + str(week) + '_'
-        for col in df.filter(regex=prefix + '.*', axis=1).columns[:200]:
+        for col in df.filter(regex=prefix + '.*', axis=1).columns[:max_assigments]:
             name = col.replace(prefix, '')
             data = df[col]
             med = data.median()
             std = data.std()
             data = data[data < med + std]
             data.hist(bins=50)
+            plt.axvline(med, color='red', linestyle='dashed', linewidth=2)
             filename = save_plot(name)
 
             report.write('#### ' + name + ' \n')
