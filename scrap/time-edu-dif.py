3d005553b348558b1c8e451c69a65748bc987919
Antti Mattila <antti.mattila@reaktor.fi>
2018-12-13 00:42:05 +0200

    Add time edu dif csv

--- scrap/time-edu-dif.py
+++ scrap/time-edu-dif.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
+df = pd.read_csv('../data/sigite2014-difficulty-data.csv', sep=';')
+key = 'SECONDS_SPENT_ON'
+names = list(map(lambda x: x.replace(key, ''), filter(lambda x: x.startswith(key), df.columns)))
+time_edu_dif = []
+
 
 def extract_time_edu_dif(row):
-    print(row)
+    for name in names:
+        time = row['SECONDS_SPENT_ON' + name]
+        education = row['EDUCATIONAL_VALUE' + name]
+        difficulty = row['DIFFICULTY' + name]
+        time_edu_dif.append([time, education, difficulty])
 
 
-df = pd.read_csv('./data/sigite2014-difficulty-data.csv', sep=';')
+df.apply(extract_time_edu_dif, axis=1)
+df2 = pd.DataFrame(time_edu_dif, columns=['time', 'education', 'difficulty'])
+df2.to_csv('../data/time_edu_dif.csv')
